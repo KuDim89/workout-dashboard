a532a4fb23ae1ea93eb56672594baa7905ae78d5
dkukharuk <dmytro.kukharuk@avenga.com>
2023-11-23 16:38:54 +0200

    add routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
+import store from "./store";
 import "./index.css";
 import { App } from "./components/App";
-import store from "./store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
@@ -13,7 +14,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );

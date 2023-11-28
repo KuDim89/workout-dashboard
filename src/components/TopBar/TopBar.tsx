083f97f2f6a74508bdfc3139e32617fda49aa08e
dkukharuk <dmytro.kukharuk@avenga.com>
2023-11-28 13:35:05 +0200

    add TopBar component

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,19 +1,20 @@
-import React, { useContext } from "react";
-import { Box, Grid, IconButton, InputBase, useTheme } from "@mui/material";
-import { ColorModeContext, tokens } from "../../theme";
+import React, { useContext } from 'react';
+import { Box, Grid, IconButton, InputBase, useTheme } from '@mui/material';
 import {
   DarkMode,
   LightMode,
   NotificationsNone,
   Search,
-} from "@mui/icons-material";
-import { useStyles } from "./styles";
-import { type ThemeMode } from "../../theme/interfaces";
+} from '@mui/icons-material';
+
+import { ColorModeContext, getColors } from '../../theme';
+import { useStyles } from './styles';
+import { type ThemeMode } from '../../theme/interfaces';
 
 export const TopBar = () => {
   const theme = useTheme();
 
-  const colors = tokens(theme.palette.mode as ThemeMode);
+  const colors = getColors(theme.palette.mode as ThemeMode);
   const classes = useStyles();
 
   // todo: should change type for colorMode variable
@@ -25,23 +26,23 @@ export const TopBar = () => {
     <Box
       className={classes.root}
       sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        px: "32px",
-        py: "10px",
+        display: 'flex',
+        justifyContent: 'space-between',
+        px: '32px',
+        py: '10px',
       }}
     >
       <Grid>Welcome, Dmytro!</Grid>
       <Box display="flex">
         <Box
           sx={{
-            display: "flex",
-            alignItems: "center",
+            display: 'flex',
+            alignItems: 'center',
           }}
         >
           <Grid onClick={colorMode.toggleColorMode}>
             <IconButton className={classes.icon}>
-              {theme.palette.mode === "dark" ? <DarkMode /> : <LightMode />}
+              {theme.palette.mode === 'dark' ? <DarkMode /> : <LightMode />}
             </IconButton>
           </Grid>
           <Grid>
@@ -52,16 +53,16 @@ export const TopBar = () => {
         </Box>
         <Grid
           sx={{
-            display: "flex",
+            display: 'flex',
             border: `1px solid ${colors.grey.DEFAULT}`,
-            borderRadius: "30px",
-            ml: "28px",
+            borderRadius: '30px',
+            ml: '28px',
           }}
         >
           <IconButton className={classes.search}>
             <Search />
           </IconButton>
-          <InputBase sx={{ py: "12px", px: "18px" }} placeholder="Search" />
+          <InputBase sx={{ py: '12px', px: '18px' }} placeholder="Search" />
         </Grid>
       </Box>
     </Box>

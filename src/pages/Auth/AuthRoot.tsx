cf687f324a299d0dec0f2290d05f48ac42d5e834
dkukharuk <dmytro.kukharuk@avenga.com>
2023-11-28 13:32:47 +0200

    update AuthRoot component

--- src/pages/Auth/AuthRoot.tsx
+++ src/pages/Auth/AuthRoot.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import { Box } from '@mui/material';
 
 import { useStyles } from './styles';
 import { RouteNames } from '../routeNames';
 import { Login } from '../../components/Login';
-import { RegisterPage } from './Register';
+import { RegisterPage } from '../../components/Register';
+import { Logo } from '../../assets/icons';
 
 export const AuthRoot = () => {
   const classes = useStyles();
@@ -13,13 +13,14 @@ export const AuthRoot = () => {
 
   return (
     <div className={classes.root}>
-      <Box className={classes.form}>
+      <form className={classes.form}>
+        <Logo className={classes.logo} />
         {location.pathname === RouteNames.LOGIN ? (
           <Login />
         ) : location.pathname === RouteNames.REGISTER ? (
           <RegisterPage />
         ) : null}
-      </Box>
+      </form>
     </div>
   );
 };
